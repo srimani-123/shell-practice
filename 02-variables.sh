7dfd6145cc76e072bd5a2070e9c793a67ec50eff
srimani <srimanikantavsa@gmail.com>
2026-01-11 16:29:57 -0500

    changes more added

--- 02-variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "robert: Hi Krishna how are you"
-echo "krishna: I am good what about you"
-
-
---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+person1=krishna
+person2=mani
+
+echo "$person1 : Hi $person2 how are you?"
+echo "$person2 : I am good what about you?"
