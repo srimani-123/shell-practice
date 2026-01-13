7a524e62dcfbb499b58348756a2fddb12d0268e9
srimani <srimanikantavsa@gmail.com>
2026-01-13 08:48:29 -0500

    done

--- 09-condition.sh
+++ 09-condition.sh
@@ -6,7 +6,7 @@ Number=$1
 #-gt, -lt, -eq, -ge, -le
 if [ $Number -gt 100 ]
 then
-    echo "Given number is greater than 100"
+    echo "Given numberr is greater than 100"
 else
     echo "Given number is less than 100"
 fi
