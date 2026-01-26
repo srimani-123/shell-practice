beb633dd56864e0bc7846428478d83dad7ccde4c
srimani <srimanikantavsa@gmail.com>
2026-01-25 21:28:29 -0500

    safh

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -8,17 +8,51 @@ then
     exit 1
 fi
 
-dnf install mysql -y
+dnf list installed mysql
 
-if [ $? -ne 0 ]
-then 
-    echo "Installing Mysql...."
+if [$? -ne 0] #not installed
+ dnf install mysql -y
+if [$? -ne 0] 
+ then 
+    echo "Installing Mysql....Failure"
     exit 1
 else
     echo "Installing mysql ...Success"
 fi
+    echo "Mysql is already.. Installed"
+fi
 
-dnf install git -y
+
+
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "Installing Mysql...."
+#     exit 1
+# else
+#     echo "Installing mysql ...Success"
+# fi
+
+dnf installed git -y
+
+if [$? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Installing Git....Failure"
+        exit 1        
+    else
+        echo "Installing Git ...Success"
+    fi
+else
+    echo "Git is already ....Installed"
+fi
+ 
+
+
+
+
 
 if [ $? -ne 0 ]
 then 
